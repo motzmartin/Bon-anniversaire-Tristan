95ed6c2623995f61034dd9ae2c84b0fe3fba464b
emmanueltudela <emmanueltudela.pro@gmail.com>
2023-05-24 18:53:49 +0200

    Added pride cloud

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// Valheim trunks
 const TREE_HEIGHT = 100;
 const TREE_FALL_SPEED = 10;
 class Trunk {
@@ -33,4 +33,38 @@ function treesFall() {
     for (let i = 0; i < NUMBER_OF_TREE_FALL; i++) {
         new Trunk(- i * 2 * TREE_HEIGHT);
     }
+}
+
+// Pride cloud cross
+const CLOUD_WIDTH = 100;
+const CLOUD_CROSS_SPEED = 10;
+class Pride_Cloud {
+    constructor() {
+        this.x = window.innerWidth + CLOUD_WIDTH;
+        this.createElement();
+        this.startAnimation();
+    }
+    startAnimation() {
+        this.interval = setInterval(() => {
+            this.x -= CLOUD_CROSS_SPEED;
+            this.htmlElement.style.left = this.x + "px";
+
+            if (this.x < -CLOUD_WIDTH) {
+                this.htmlElement.remove();
+                clearInterval(this.interval);
+            }
+        }, 1000 / 60);
+    }
+    createElement() {
+        this.htmlElement = document.createElement("img");
+        this.htmlElement.src = "res/img/pride_cloud.gif";
+        this.htmlElement.style.position = "fixed";
+        this.htmlElement.style.left = this.x + "px";
+        this.htmlElement.style.top = "50%";
+        this.htmlElement.style.transform = "translate(-50%, -50%)";
+        document.body.append(this.htmlElement);
+    }
+}
+function prideCloudCross() {
+    new Pride_Cloud();
 }
